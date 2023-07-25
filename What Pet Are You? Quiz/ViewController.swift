c51f09d6f4247b7a001298c2c8f3aedeaee1922a
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-07-25 11:09:30 +0300

    Initial Commit

---
+++ What Pet Are You? Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  What Pet Are You? Quiz
+//
+//  Created by Kate Kashko on 25.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
