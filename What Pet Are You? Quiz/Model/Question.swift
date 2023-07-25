1f7c346b7f3031789c5d90196cd865180cb06516
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-07-25 12:30:51 +0300

    Add MVC

---
+++ What Pet Are You? Quiz/Model/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  What Pet Are You? Quiz
+//
+//  Created by Kate Kashko on 25.07.2023.
+//
+
+import Foundation
---
+++ What Pet Are You? Quiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultViewController.swift
+//  What Pet Are You? Quiz
+//
+//  Created by Kate Kashko on 25.07.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
